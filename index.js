6f802d3fb8ce582a8ce3a32e8ef644167d7aebcd
Lars_HD44 <56442962+LarsHD44@users.noreply.github.com>
2021-05-06 19:44:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,40 +7,40 @@ var client = new Discord.Client()
 
 client.on('ready', () => {
     console.log(`Eingeloggt als ${client.user.tag}...`)
-    client.user.setActivity(`DevSchuppen managen`);
+    client.user.setActivity(`with servers`);
 })
 
 client.on('guildMemberAdd', member => {
     // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch => ch.name === 'willkommen');
+    const channel = member.guild.channels.find(ch => ch.name === 'welcome');
     // Do nothing if the channel wasn't found on this server
     if (!channel) return;
     // Send the message, mentioning the member
-    channel.send(`${member} Ist jetzt Mitglied des **DevSchuppen**!`);
+    channel.send(`${member} Is now a member of the **Server**!`);
   });
 
 client.on('message', message => {
     if (message.content === '+myavatar') {
-        message.channel.send('Das ist dein Avatar:', message.author.avatarURL);
+        message.channel.send('This is your Avatar:', message.author.avatarURL);
     }
 });
 
 const help = new Discord.RichEmbed()
 	.setColor('#0099ff')
 	.setTitle('Help')
-	.setURL('http://devschuppen.bplaced.net/GB.html')
+	.setURL('http://bit.ly/GB_Bot')
 	.setAuthor('GameBoy', 'https://media.discordapp.net/attachments/643320294450266128/680894663657521169/GameBoy.png', 'http://devschuppen.bplaced.net/GB.html')
 	.setDescription('Commands:')
 	.setThumbnail('')
 	.addField('Help', '+help')
 	.addField('say', '+say', true)
 	.addField('kick', '+kick', true)
-  .addField('ban', '+ban', true)
-  .addBlankField()
-  .addField('Um alle Commands anzusehen klicke hier:', 'http://bit.ly/GB-Bot', true)
-  .addBlankField()
-  .addField('Support-Server:', 'https://discord.gg/C7sjRgC', true)
-  .setImage('')
+  	.addField('ban', '+ban', true)
+  	.addBlankField()
+  	.addField('To see all commands click here:', 'http://bit.ly/GB_Bot', true)
+  	.addBlankField()
+  	.addField('Support-Server:', 'https://discord.gg/C7sjRgC', true)
+  	.setImage('')
 	.setTimestamp()
 	.setFooter('GameBoy by Lars_HD44', 'https://media.discordapp.net/attachments/643320294450266128/680894663657521169/GameBoy.png');
 
@@ -75,7 +75,7 @@ client.on('message', message => {
       // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
       // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
       const m = await message.channel.send("Ping?");
-      m.edit(`Pong! Die Latenz beträgt ${m.createdTimestamp - message.createdTimestamp}ms. Die API-Latenz beträgt ${Math.round(client.ping)}ms`);
+      m.edit(`Pong! The Latency is ${m.createdTimestamp - message.createdTimestamp}ms. The API-Latency is ${Math.round(client.ping)}ms`);
     }
     
     if(command === "say") {
@@ -93,26 +93,26 @@ client.on('message', message => {
       // Please read on Array.some() to understand this bit: 
       // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
       if(!message.member.roles.some(r=>["Owner", "Admin"].includes(r.name)) )
-        return message.reply("Sorry, dazu hast du keine rechte!");
+        return message.reply("Sorry, you dont have the required rights!");
       
       // Let's first check if we have a member and if we can kick them!
       // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
       // We can also support getting the member by ID, which would be args[0]
       let member = message.mentions.members.first() || message.guild.members.get(args[0]);
       if(!member)
-        return message.reply("Bitte erwähne Jemanden!");
+        return message.reply("Please mention somebody!");
       if(!member.kickable) 
-        return message.reply("Ich kann diesen User nicht kicken! Hat dieser User eine höhere Rolle? Habe ich Kickrechte?");
+        return message.reply("I cant kick this User! Have this User a higher role? Have I rights to kick somebody?");
       
       // slice(1) removes the first part, which here should be the user mention or ID
       // join(' ') takes all the various parts to make it a single string.
       let reason = args.slice(1).join(' ');
-      if(!reason) reason = "Kein Grund angegeben!";
+      if(!reason) reason = "No reason provided!";
       
       // Now, time for a swift kick in the nuts!
       await member.kick(reason)
-        .catch(error => message.reply(`Sorry ${message.author} Ich konnte den User nicht kicken weil: ${error}`));
-      message.reply(`${member.user.tag} wurde von ${message.author.tag} gekickt weil: ${reason}`);
+        .catch(error => message.reply(`Sorry ${message.author} I cant kick this User because: ${error}`));
+      message.reply(`${member.user.tag} was kicked by ${message.author.tag} because: ${reason}`);
   
     }
     
@@ -120,20 +120,20 @@ client.on('message', message => {
       // Most of this command is identical to kick, except that here we'll only let admins do it.
       // In the real world mods could ban too, but this is just an example, right? ;)
       if(!message.member.roles.some(r=>["Owner"].includes(r.name)) )
-        return message.reply("Sorry, dazu hast du keine rechte");
+        return message.reply("Sorry, you dont have the required rights!");
       
       let member = message.mentions.members.first();
       if(!member)
-        return message.reply("Bitte erwähne jemanden!");
+        return message.reply("Please mention somebody!");
       if(!member.bannable) 
-        return message.reply("Ich kann diesen User nicht bannen! Hat dieser User eine höhere Rolle? Habe ich Banrechte?");
+        return message.reply("I cant ban this User! Have this User a higher role? Have I rights to ban somebody?");
   
       let reason = args.slice(1).join(' ');
-      if(!reason) reason = "Kein Grund Angegeben!";
+      if(!reason) reason = "No reason provided!";
       
       await member.ban(reason)
-        .catch(error => message.reply(`Sorry ${message.author} Ich kann keinen bannen weil: ${error}`));
-      message.reply(`${member.user.tag} wurde von ${message.author.tag} gebannt weil: ${reason}`);
+        .catch(error => message.reply(`Sorry ${message.author} I cant ban this User because: ${error}`));
+      message.reply(`${member.user.tag} was banned by ${message.author.tag} because: ${reason}`);
     }
     
     if(command === "purge") {
@@ -144,12 +144,12 @@ client.on('message', message => {
       
       // Ooooh nice, combined conditions. <3
       if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-        return message.reply("Bitte tippe eine Nummer von 2-100 ein!");
+        return message.reply("Please type a number from 2-100!");
       
       // So we get our messages, and delete them. Simple enough, right?
       const fetched = await message.channel.fetchMessages({limit: deleteCount});
       message.channel.bulkDelete(fetched)
-        .catch(error => message.reply(`Konnte keine Nachichten löschen weil: ${error}`));
+        .catch(error => message.reply(`I cant delete the messages because: ${error}`));
     }
   });
   
